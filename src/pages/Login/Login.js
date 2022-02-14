ec5326861d19c55a6b1c4ef4fe283e975c859b87
joscord <josecorderodev@gmail.com>
2022-02-14 11:54:28 -0300

    Created pages directory and Home.js, Login.js and Signup.js along with their stylesheets using css modules

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import './Login.module.css'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,9 @@
+import './Signup.module.css';
+
+const Signup = () => {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
