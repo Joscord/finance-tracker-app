6787b8eee268a94dc53b7c82b586a470dd7ead73
joscord <josecorderodev@gmail.com>
2022-02-14 17:01:37 -0300

    Modified AutContext.js, useAuthContext.js and useSignup.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,31 +1,32 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react';
 
-// Estado por defecto/inicial
 const defaultState = {
-    user: null
-}
+	user: null,
+};
 
-// Función reductora
 export const authReducer = (state, action) => {
-    switch(action.type) {
-        // Aquí irán los casos, por ahora dejaremos sólo el caso por default
-        default: 
-            return state;  
-    }
-}
+	switch (action.type) {
+		// Definimos un caso para la acción de LOG_IN
+		case 'LOGIN':
+			return {
+				// Nótese que usando el spread operator buscamos copiar todas las propiedades que habría en el estado anterior, en el caso de existir más propoiedades
+				...state,
+				// El valor de la prop user será lo que venga por payload
+				user: action.payload,
+			};
+		default:
+			return state;
+	}
+};
 
-// Creamos un contexto con la función createContext();
 export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
-    // Invocamos useReducer pasando la función reductora y el estado inicial. Esto nos devuelve el estado y la función para despachar acciones
-    const [state, dispatch] = useReducer(authReducer, defaultState);
+export const AuthContextProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(authReducer, defaultState);
 
-    return (
-    // Usamos la prop value para disponibilizar el estado a un árbol de componentes. Pasamos un objeto con las propiedades de estado (por ahora sólo user) y la función para despachar acciones
-    <AuthContext.Provider value={{...state. dispatch}}>
-        {children}
-    </AuthContext.Provider>
-    )
-}
-
+	return (
+		<AuthContext.Provider value={{ ...state.dispatch }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -1,9 +1,14 @@
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
-const useAuthContext = () => {
+export const useAuthContext = () => {
     const authContext = useContext(AuthContext);
+    // Hacemos un checkeo por si no tenemos el contexto. Básicamente si no tiene un valor es porque estamos fuera del scope del proveedor   
+    if (!authContext) {
+        throw new Error('useAuthContext must be inside an AuthContextProvider');
+    }
     return {
         authContext
     }
 }
+
