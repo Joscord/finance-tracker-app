a41dd61a7f5615399d059374d361182ba3d81807
joscord <josecorderodev@gmail.com>
2022-02-14 15:53:24 -0300

    Added firebase as a dependency. Modified Signup.js to use useSignup custom hook

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,61 +1,75 @@
 import { useState } from 'react';
 import styles from './Signup.module.css';
+import  useSignup  from '../../hooks/useSignup';
 
 const Signup = () => {
-  // Manejaremos 4 piezas de estado: nombre, email, password y confirmación de password
-  const [displayName, setDisplayName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+	const [displayName, setDisplayName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	// Importamos el custom hook useSignup. Recordemos que el hook en sí no recibe ningún argumento sino que lo recibe la función signup
+	const { error, isPending, signup } = useSignup();  
 
-  // Crearemos una función para validar que los passwords sean iguales
-  const validatePassword = (password, confirmPassword) => password === confirmPassword;
+	const validatePassword = (password, confirmPassword) =>
+		password === confirmPassword;
 
-  // Definimos la función para el envío del formulario 
-  const handleSubmit = e => {
-    e.preventDefault();
-    // Usamos la función para validar password, si no coinciden entonces no seguimos con el envío
-    if (!validatePassword(password, confirmPassword)) {
-      alert('Passwords do not match')
-      setPassword('');
-      setConfirmPassword('');
-      return;
-    }
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (!validatePassword(password, confirmPassword)) {
+			alert('Passwords do not match');
+			setPassword('');
+			setConfirmPassword('');
+			return;
+		}
+    signup(email, password, displayName);
+    // Reseteamos los campos luego del registro
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setDisplayName('');
 
-  }
-  return (
-    <form onSubmit={handleSubmit} className={styles['signup-form']}>
-      <label>
-        <span>Enter your name</span>
-        <input 
-        type='text'
-        onChange={e => setDisplayName(e.target.value)}
-        value={displayName}/>
-      </label>
-      <label>
-        <span>Enter your email</span>
-        <input 
-        type='email'
-        onChange={e => setEmail(e.target.value)}
-        value={email}/>
-      </label>
-      <label>
-        <span>Enter your password</span>
-        <input 
-        type='password'
-        onChange={e => setPassword(e.target.value)}
-        value={password}/>
-      </label>
-      <label>
-        <span>Confirm your password</span>
-        <input 
-        type='password'
-        onChange={e => setConfirmPassword(e.target.value)}
-        value={confirmPassword}/>
-      </label>
-      <button className='btn'>Sign Up</button>
-    </form>
-  )
-}
+	};
+	return (
+		<form onSubmit={handleSubmit} className={styles['signup-form']}>
+			<label>
+				<span>Enter your name</span>
+				<input
+					type='text'
+					onChange={e => setDisplayName(e.target.value)}
+					value={displayName}
+				/>
+			</label>
+			<label>
+				<span>Enter your email</span>
+				<input
+					type='email'
+					onChange={e => setEmail(e.target.value)}
+					value={email}
+				/>
+			</label>
+			<label>
+				<span>Enter your password</span>
+				<input
+					type='password'
+					onChange={e => setPassword(e.target.value)}
+					value={password}
+				/>
+			</label>
+			<label>
+				<span>Confirm your password</span>
+				<input
+					type='password'
+					onChange={e => setConfirmPassword(e.target.value)}
+					value={confirmPassword}
+				/>
+			</label>
+      {/* Mostramos este botón cuando isPending es false */}
+			{!isPending && <button className='btn'>Sign Up</button>}
+      {/* Usamos el estado de error para mostrar si hay error */}
+      {isPending && <button className='btn' disabled>Loading</button>}
+      {error && <p>{error}</p>}
+		</form> 
+	);
+};
 
-export default Signup
+export default Signup;
