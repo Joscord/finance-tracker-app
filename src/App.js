5a8cf059c8cdec8daafd819da313a77f5973d519
joscord <josecorderodev@gmail.com>
2022-02-14 12:02:41 -0300

    Defined home, login and signup routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+} from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+	return (
+		<div className='app'>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route path='/login' component={Login} />
+					<Route path='/signup' component={Signup} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
