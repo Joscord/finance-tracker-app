de097a431e329aad183fb3a4569bd722e860fc52
joscord <josecorderodev@gmail.com>
2022-02-15 16:39:28 -0300

    Modified Navbar.js to include conditional rendering. Modified auth context

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,40 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
-import { useLogout }  from '../../hooks/useLogout';
+import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 const Navbar = () => {
-	// De nuestro custom hook vamos a destructurar la función de logout
-	const { logout } = useLogout(); 
+	const { logout } = useLogout();
+	// Importamos el hook para utilizar nuestro contexto. Recordemos que de authContexnt obtenemos el objeto value que tiene la prop user el valor del estado. Este estado tiene una propiedad user cuyo valor nos indica si el usuario está o no conectado
+	const { user } = useAuthContext();
+
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li className={styles.title}>Finance App</li>
-				<li>
-					<Link to={'/login'}>Login</Link>
-				</li>
-				<li>
-					<Link to={'/signup'}>Signup</Link>
-				</li>
-        <li>
-          {/* Creamos un botón para el logout, asociando la función de logout al evento click */}
-          <button className='btn' onClick={logout}>Logout</button>
-        </li> 
+				{/* Mostramos el botón de login de manera condicional si el usuario no está conectado (user: null) */}
+				{!user && (
+					// Nótese que usamos un fragmento porque sólo podemos retornar un elemento raíz
+					<>
+						<li>
+							<Link to={'/login'}>Login</Link>
+						</li>
+						<li>
+							<Link to={'/signup'}>Signup</Link>
+						</li>
+					</>
+				)}
+				{/*Si el usuario está conectado mostramos el botón de  logout además de su nombre (que es una propiedad que habíamos añadido antes)*/}
+				{user && (
+					<>
+						<li>Hello {user.displayName}</li>
+						<li>
+							<button className='btn' onClick={logout}>
+								Logout
+							</button>
+						</li>
+					</>
+				)}
 			</ul>
 		</nav>
 	);
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,11 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
+// Importamos el objeto del servicio de autenticación de Firebase
+import { projectAuth } from '../firebase/config';
 
+// Nuestro estado inicial ahora incluye una nueva propiedad llamada authIsReady. El valor inicial de esta prop es false y básicamente lo que haremos con esta es evaluarla en el componente App e indicar que no queremos mostrar ningún componente a menos que sea true.
 const defaultState = {
 	user: null,
+	authIsReady: false,
 };
 
 export const authReducer = (state, action) => {
@@ -11,9 +15,11 @@ export const authReducer = (state, action) => {
 				...state,
 				user: action.payload,
 			};
-        case 'LOGOUT':
-            // Simplemente retornamos el estado inicial/por defecto donde la prop user es null
-            return defaultState;
+		case 'LOGOUT':
+			return { ...state, user: null };
+			// Añadimos la acción de AUTH_IS_READY. Nótese que también volvemos la prop authIsReady: true para indicar que se hizo el checkeo de la autenticación
+		case 'AUTH IS READY':
+			return { ...state, user: action.payload, authIsReady: true };
 		default:
 			return state;
 	}
@@ -23,6 +29,18 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(authReducer, defaultState);
+	// Queremos correr una función cuando el componente sea evaluado por primera vez, por lo que usaremos useEffect
+	useEffect(() => {
+		// Usamos el método onAuthStateChanged. Esta función se comunica con Firebase y básicamente dice: "avísanos si existe un cambio en el estado de autenticación y cuando haya quiero que dispares esta función". Esta función recibe el usuario como parámetro, por lo tanto, haremos que cuando ocurra un cambio actualicemos nuestro estado en la app de usuario. Nótese que este método nos retorna una función para desuscribirnos.
+		const unsub = projectAuth.onAuthStateChanged(user => {
+			// Cuando corre esta función entonces por primera vez podemos despachar una acción. El payload es el usuario, si firebase encuentra a uno entonces envía ese usuario como respuesta y si no es nulo
+			dispatch({ type: 'AUTH_IS_READY', payload: user });
+			// La función onAuthStateChanged se va a disparar siempre que ocurra un login o un logout, pero sólo queremos hacer esa revisión inicial. Por lo tanto debemos cancelar esa suscripción y lo hacemos con la función unsub que retorna. Así nunca volverá a ser llamada la función. Nótese que no hacemos una función de cleanup porque no se trata de correr una función mientras se desmonta el componente, esto ocurre antes de que se monte cualquiera
+			unsub();
+		});
+
+		// Pasamos un arreglo vacío como dependencia para indicar que sólo correremos la función cuando cargue por primera vez
+	}, []);
 
 	return (
 		<AuthContext.Provider value={{ ...state, dispatch }}>
