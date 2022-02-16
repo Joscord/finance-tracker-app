7569000a330ae8834918cc164c79f30a7a21a007
joscord <josecorderodev@gmail.com>
2022-02-15 22:30:42 -0300

    Modified Home.js and created TransactionForm.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import styles from './Home.module.css';
 
 const Home = () => {
-	return <div className={styles.container}>
-		<div className={styles.content}>
-
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}></div>
+			<div className={styles.sidebar}></div>
 		</div>
-		<div className={styles.sidebar}>
-
-		</div>
-		</div>;
+	);
 };
 
 export default Home;
---
+++ src/pages/Home/TransactionForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionForm = () => {
+  return (
+    <div>TransactionForm</div>
+  )
+}
+
+export default TransactionForm
